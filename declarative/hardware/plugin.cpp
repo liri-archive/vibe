9969369440d64566feca15d467857e3a5fa6e1ee
Pier Luigi Fiorini <pierluigi.fiorini@hawaiios.org>
2016-07-18 22:40:11 +0200

    Rename hardware QML plugin
    Rename to Hawaii.Hardware to follow other plugins naming.
    
    Issue: #2

--- declarative/hardware/plugin.cpp
+++ declarative/hardware/plugin.cpp
@@ -35,8 +35,8 @@ class HardwarePlugin : public QQmlExtensionPlugin
 public:
     void registerTypes(const char *uri)
     {
-        // @uri org.hawaiios.hardware
-        Q_ASSERT(uri == QStringLiteral("org.hawaiios.hardware"));
+        // @uri Hawaii.Hardware
+        Q_ASSERT(uri == QStringLiteral("Hawaii.Hardware"));
 
         qmlRegisterType<HardwareEngine>(uri, 0, 1, "HardwareEngine");
         qmlRegisterUncreatableType<Battery>(uri, 0, 1, "Battery",
